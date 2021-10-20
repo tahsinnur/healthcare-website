10d68a5ff1df385e6f3c388f507fc597c2cd255e
tahsinnur <tahsinnurtajin@gmail.com>
2021-10-20 21:46:02 +0600

    banner img changed

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -4,12 +4,15 @@ import B1 from '../../../images/Banner/Banner1.jpg'
 import B2 from '../../../images/Banner/Banner2.jpg';
 import B3 from '../../../images/Banner/Banner3.jpg';
 
+import './Banner.css';
+
 const Banner = () => {
     return (
         <Container fluid className="p-0">
             <Carousel fade>
                 <Carousel.Item>
                     <img
+                        height="700px"
                         className="d-block w-100"
                         src={B1}
                         alt="First slide"
@@ -20,6 +23,7 @@ const Banner = () => {
                 </Carousel.Item>
                 <Carousel.Item>
                     <img
+                        height="700px"
                         className="d-block w-100"
                         src={B2}
                         alt="Second slide"
@@ -31,6 +35,7 @@ const Banner = () => {
                 </Carousel.Item>
                 <Carousel.Item>
                     <img
+                        height="700px"
                         className="d-block w-100"
                         src={B3}
                         alt="Third slide"
